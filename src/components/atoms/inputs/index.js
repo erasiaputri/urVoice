a0cce49384b8f2d0c262c8bfcdd29bc63f1db4e3
clifordrmndr <s11810126@student.unklab.ac.id>
2021-05-14 06:10:30 +0800

    configure sign up to firebase

--- src/components/atoms/inputs/index.js
+++ src/components/atoms/inputs/index.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import {TextInput,View,StyleSheet} from 'react-native'
 
-const Inputs = ({holder,bottom}) => {
+const Inputs = ({holder,bottom,value,onChangeText,val}) => {
   const style = StyleSheet.create({
     input:{
       height:60,
       width:337,
       borderRadius:50,
       textAlign:'center',
+      alignItems:'center',
       borderWidth:1,
       marginBottom:bottom
     }
   })
   return (
-    <TextInput placeholder={holder} style={style.input}/>
+    <TextInput placeholder={holder} style={style.input} value={value} onChangeText={onChangeText} secureTextEntry={val}/>
   )
 }
 
